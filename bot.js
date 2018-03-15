49132042f863826e8bb66da0a81ca529c12d82ce
nikgr371 <nikgr371@student.liu.se>
2018-03-15 13:25:15 +0100

    Added 200% more Hadoque

--- bot.js
+++ bot.js
@@ -8,9 +8,10 @@ var tyst;
 const bot = new Discord.Client();
 bot.on("ready", () => {
 	console.log("Skrothög startad");
+	console.log("Sluta spela Realm Grinder, " + haddock() + "!");
 	bot.user.setPresence({
 		game: {
-		name: `Hatar på Realm Grinder`,
+		name: "Hatar på Realm Grinder",
 		type: 0
    }
 	});
@@ -20,11 +21,11 @@ bot.on("ready", () => {
 		console.log("Tyst läge");
 	} else {
 		tyst = false;
-		console.log("Inte tyst läge");
+		console.log("Högljutt läge");
 	}
 	
 	if(!tyst) {
-		gaffla.send("Nu hatar vi lite på Realm Grinder!");
+		gaffla.send("No Realm Grinder bot! Nu med Haddock-mode!");
 	}
 	
 	
@@ -36,12 +37,15 @@ bot.on("presenceUpdate", async (oldMember, newMember) => {
 	if (newMember.presence.game !== null) {
 		console.log(newMember.nickname + ": " + newMember.presence.game.name);
 		if( newMember.presence.game.name == "Realm Grinder") {
+			console.log(newMember.nickname + " kör Realm Grinder");
 			spam(newMember.user);
+			if(!tyst) {
+				console.log("Jag sade det högt");
+				gaffla.send(newMember.nickname + " spelar lite Realm Grinder, sprid lite hat till den personen!");
+		}
 			
 		}
-		if(!tyst) {
-			gaffla.send(newMember.nickName + " Spelar lite Realm Grinder, sprid lite hat till den personen!");
-		}
+
 	}
 
 	
@@ -50,13 +54,15 @@ bot.on("presenceUpdate", async (oldMember, newMember) => {
 
 function haddock() {
 	//http://www.nissepedia.com/index.php/Kapten_Haddocks_samlade_svordomar
-	var fraser = {"murmeldjur","enögda murmeldjur", "sjögurka", "apsvansade analfabet", "amöba"};
-	return "";
+	var fraser = ["ditt murmeldjur","ditt enögda murmeldjur", "din sjögurka", "din apsvansade analfabet", "din amöba", "din blåkullatomte", "ditt blötdjur", "din bondlurk", "din deghög", "din drummel", "din dyngspridare", "ditt eländiga kryp", "din enögda kannibal", "din erbarmliga plattfot", "din fähund", "din fega ynkrygg", "din fåntratt", "ditt fördömda kräk", "din förpiskade luspudel", "din gamla knölsvan", "din gnom", "din grobian", "din grottmänniska", "din huggormsavföda", "din idiot", "din insjögangster", "ditt jäkla odjur", "ditt kloakdjur", "din kloakråtta", "din kramsfågelmördare", "din kryddkrämare", "din luspudel", "din lymmel", "din ockrare", "din odugling", "din parasit", "din pestråtta", "din pillerbagge", "din pottsork", "din pyromanapa", "din råttsvans", "ditt rötägg", "din sakramentskade sumprunkare", "din sladderfågel", "din sillmjölk", "din skabbhals", "din skabbråtta", "din skurk", "ditt skunkdjur", "ditt slyn-yngel", "din snorvalp", "din sopprot", "din soppråtta", "din sötvattenspirat", "din tjockskalle", "din tjurskalle", "ditt tryffelsvin", "din tångräka", "din usling", "din vagabond", "din vandal", "din vegetarian", "din vidriga apmänniska", "din vidriga varulv", "din vinlus", "din vrakplundrare", "din vrålapa", "ditt vårtsvin", "din åsneskalle", "din ärkebandit", "din ärkelögnare"];
+	var rand = fraser[Math.floor(Math.random() * fraser.length)];
+	return rand;
+
 }
 
 function spam(user) {
 			for(i = 0; i < 5; i++) {
-				user.send("Sluta spela Realm Grinder!");
+				user.send("Sluta spela Realm Grinder, " + haddock() + "!");
 			}
 }
 
