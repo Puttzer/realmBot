5e91fb4013e815933fb67618009e1f793d08c6e6
Viktor Palm <viktor.palm.vp@gmail.com>
2020-12-04 21:48:50 +0100

    lite extra kommandon

--- bot.js
+++ bot.js
@@ -37,7 +37,8 @@ const fucked = "https://www.youtube.com/watch?v=ptw2Cor2HQM&feature=youtu.be";
 //const coom = "https://www.youtube.com/watch?v=IL1bityU5DA";
 const drill = "https://www.youtube.com/watch?v=zjnJk5V9nSM&feature=youtu.be";
 const viktor_video = "https://www.youtube.com/watch?v=2Bjy5YQ5xPc";
-const carrahager_video = "https://www.youtube.com/watch?v=ZeDTeMjVc18";
+const carragher_video= "https://www.youtube.com/watch?v=ZeDTeMjVc18";
+const karlsson_video = "https://www.youtube.com/watch?v=W8kwT0QFAU4";
 const inga = "inga.mp3";
 
 const emmaLjud = "https://www.youtube.com/watch?v=Gb2jGy76v0Y";
@@ -488,12 +489,19 @@ bot.on("ready", () => {
 				}
 
 			}
-			else if (msg.content.toLowerCase() === "!carrhager") {
-				
-				msg.reply(""Ska is long before reggae, you know ska came before reggae, But you know reggae came from ska\" - Bruce Greene");
+			else if (msg.content.toLowerCase() === "!carragher") {
+				msg.reply("\"Ska is long before reggae, you know ska came before reggae, But you know reggae came from ska\" - Bruce Greene");
 				if(!playing) {
 					playing = true
-					soundplayer(carrhager_video, channel);
+					soundplayer(carragher_video, channel);
+				}
+			}
+			else if (msg.content.toLowerCase() === "!karlsson") {
+				msg.reply("\" Någon annan sprutar på mig just nu ;)\"" - Niklas Granberg");
+				msg.reply("\"Älskar sperma på mig!!!! cummies\"" - Niklas Granberg");
+				if(!playing) {
+					playing = true
+					soundplayer(karlsson_video, channel);
 				}
 			}
 			else if (cmd[0] === "!play") {
